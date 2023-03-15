46240bc7496da5537535d2072c6c9890ca70e8de
Juan <juangrondona.01@gmail.com>
2023-03-15 19:48:17 -0300

    Se agrega control de vacio en edicion marca.

--- src/componentes/Marcas/EditarMarcas.jsx
+++ src/componentes/Marcas/EditarMarcas.jsx
@@ -3,82 +3,89 @@ import { Link, useParams } from "react-router-dom";
 import * as API from "../../servicios/servicio";
 
 export function EditarMarcas() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [mensajeSuccess, setmensajeSuccess] = useState("");
   const [nombre, setNombre] = useState("");
   const [estado, setEstado] = useState("");
- 
+  const [Error, setError] = useState("");
 
-  useEffect(() => {trae_datos(id) }, []);
+
+  useEffect(() => { trae_datos(id) }, []);
 
   const trae_datos = async () => {
-    // event.preventDefault();
     const datos_marca = await API.getMarcaById(id)
     console.log(datos_marca)
     setNombre(datos_marca.nombre)
     setEstado(datos_marca.estado)
   };
 
+  ///// esto evita que se cargue la edición en vacio //// 
   const editar_marca = () => {
+    if (nombre.trim() === '' || estado.trim() === '') {
+      setError(true);
+      alert("No se permite enviar vacio en edicion");
+      return;
+    }
+    /////// hasta aca //////
+
     const datos_enviar = {
       nombre: nombre,
       estado: estado
     };
     API.UpdateMarcas(id, datos_enviar);
-    // nombre_marca.current.value=null;
 
     setmensajeSuccess("Se Edito la marca");
     setTimeout(() => {
       setmensajeSuccess("");
-      
+
       console.log("actualiza datos Marcas", datos_enviar);
     }, 2000);
   };
   return (
     <>
-    <div className="card">
-      <div className="card-header">Edicion de la marca</div>
-      {mensajeSuccess ? (
-        <div class="alert alert-success" role="alert">
-          {mensajeSuccess}
-        </div>
-      ) : (
-        ""
-      )}
-      <div className="card-body">
-        <div className="form-group">
-          <label for="">NOMBRE</label>
-          <input
-            type="text"
-            value={nombre}
-            onChange={(event) => setNombre(event.target.value)}
-            name=""
-            id=""
-            className="form-control"
-            
-            aria-describedby="helpId"
-          />
-          <small id="helpId" className="text-muted">
-            &nbsp;
-          </small>
-        </div>{" "}
-        <div className="form-group">
-          <button
-            onClick={editar_marca}
-            type="button"
-            className="btn btn-primary"
-          >
-            Guardar
-          </button>
-          <Link to={"/marcas "}>
-            <button type="button" className="btn btn-secondary">
-              Volver al listado
+      <div className="card">
+        <div className="card-header">Edicion de la marca</div>
+        {mensajeSuccess ? (
+          <div class="alert alert-success" role="alert">
+            {mensajeSuccess}
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="card-body">
+          <div className="form-group">
+            <label for="">NOMBRE</label>
+            <input
+              type="text"
+              value={nombre}
+              onChange={(event) => setNombre(event.target.value)}
+              name=""
+              id=""
+              className="form-control"
+
+              aria-describedby="helpId"
+            />
+            <small id="helpId" className="text-muted">
+              &nbsp;
+            </small>
+          </div>{" "}
+          <div className="form-group">
+            <button
+              onClick={editar_marca}
+              type="button"
+              className="btn btn-primary"
+            >
+              Guardar
             </button>
-          </Link>
+            <Link to={"/marcas "}>
+              <button type="button" className="btn btn-secondary">
+                Volver al listado
+              </button>
+            </Link>
+          </div>
         </div>
+        <div className="card-footer text-muted">Bazar Capicua</div>
       </div>
-      <div className="card-footer text-muted">Bazar Capicua</div>
-    </div>
     </>
   );
 }
